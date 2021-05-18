a4c2ed12a33fd8b830f9cab378f4caade92ccbc7
jimrothstein <jimrothstein@gmail.com>
2021-05-17 23:32:57 -0700

    TAGS:  first commit

---
+++ R/TAGS_utils.R
@@ -0,0 +1,20 @@
+#   
+#   #'  NOTATION:   Following Hadley (purrr:::) refer to `list` as list and refer
+#     #'  to `atomic vectors` as character vectors, numeric vectors etc ....
+#     #'  FOR BREVITY:    chr[] refers to character vector,  chr[n] refers to
+#     #'  character vector where length of each element is n.
+#     #'
+#     #'
+#   '#  PURPOSE:   From all *.R, *.RMD files in a folder, extract `TAGS:`, a yaml
+#     ###  header and put into database
+#   
+#   ' ### REF
+#   ' # from bookdown::   file=utils.R
+#   
+#   
+#   
+#   
+#   rmd_pattern  <- '[.][Rr](md|markdown)$'
+#   md_pattern  <- '[.][Rr]?(md|markdown)$'
+#   
+
