7a169e37ce307fac0c693645d2b7fa0ca6625f06
jimrothstein <jimrothstein@gmail.com>
2021-10-22 10:00:40 -0700

    wip

--- tests/testthat/test_generate_TAGS_database.R
+++ tests/testthat/test_generate_TAGS_database.R
@@ -1,176 +1,209 @@
+---
+#title: "`r knitr::current_input()`"
+#date: "`r paste('last updated', 
+#    format(lubridate::now(), ' %d %B %Y'))`"
+output:   
+  html_document:  
+        code_folding: show
+        toc: true 
+        toc_depth: 4
+        toc_float: true
+        mathjax: default
+  pdf_document:   
+    latex_engine: xelatex  
+    toc: true
+    toc_depth:  4   
+fontsize: 10pt   
+geometry: margin=0.4in,top=0.25in   
+TAGS:  
+---
+
+\small
+
+```
+- Use ticks:
+- keep YAML, but remove title, date
+
+!pandoc % -f markdown -V fontsize=10 -o ~/Downloads/print_and_delete/out.pdf
+
 # file <- "tests/testthat/test_generate_TAGS_database.R"
 
 ## Definitions 
 #  
-# * `the_files` = named char[] of all files in the_dir which match the_pattern.  
-# * `the_dir` TAGS db will come from the_files in this directory.
-# *  the_pattern  chr[1] regex pattern.
-# * `the_yaml` = named list of yaml, one element for each file in the_files.  Content is char[], one line for each line of yaml.
+# * `the_files` = named character vector of  files which match the_pattern.  
+# * `the_dir` = string; source directory for files.
+# *  the_pattern string; regex (PERL) pattern.
+# * `the_yaml` = named list, one element for each file.  Content is character
+# vector; one line for each line of yaml in the file.  If file has no yaml,
+# character vector is TODO.
 # * `TAGS` = named chr[],  each element has name = its file name; contents are NA if file has no yaml or 
 #   chr[1] of contents of `TAGS:` line ( `tag1,tag2,tag3...` )
-
+#
 
 
 #  load_all()
-#   Be sure in vim and R in same wd
-#    USE   ,rd  setwd("~/code/publish_project/rmd/")
 {
   load_all()
   library(data.table)
-  library(purrr)
-  library(kableExtra)
-  options(datatable.prettyprint.char=60L)
 }
 
-# FOR testing ##########
+# source directory
 the_dir = "~/code/try_things_here/rmd"
 
-{ ## begin
+## begin
 
 begin  <- Sys.time()
 
-  { ## get_files
-
-      ## ENDS in either md or markdown; preceded by one of .R .r .
-      ## ? = 0,1
-      rmd_pattern  <- '[.][Rr](md|markdown)$'
-      md_pattern  <- '[.][Rr]?(md|markdown)$'
-      the_pattern  <- md_pattern
-
-      ## get files (omit files ^_),returns named char[]
+  { ## RETURN `the_files` 
+      ## ENDS in either md, R, Rmd  (R or r) 
+      ## Omit ^_ files
+      the_pattern  <- '[.]([Rr]|md|[Rr]md)$'   # no spaces
       the_files  <- get_files(path = the_dir, pattern = the_pattern)
-
-      ## TODO nicely format
-      head(the_files)
+      the_files
   }
 
-{  ## extract yaml for each file
+  
+    ## RETURN `the_yaml` named list; each element represents one file.  If that
+##    file has proper yaml and at least one line (TAGS:) there will be non-empty value in character vector.
 
-  #
-  # For each file, extract yaml contents.   into named list:
-  # lapply preserves names, if exist.  
-  # the_files is named char[] and so the_yaml is named list
-  #
-  # each element of \code{}the_yaml} holds a char[], yaml lines of each file.
-  # # the_files is named char[]
-    ## ERROR !   if file has TAGS:  but no content, empty.
-    ## ie TAGS:
-    ##
-    ## Yihui
-    #the_yaml  <- lapply(the_files, get_yaml, dir=the_dir )
-    #
     ## ymlthis:::
     the_function  <- function(x) {
         ymlthis:::read_rmd(paste0(the_dir,"/",x), output=c("frontmatter"))
     }
-    the_yaml   <- lapply(the_files, the_function)
-    the_yaml
+    
+    
+    x.1   <- lapply(the_files, the_function)
+    x.1
+    x.2  <- lapply(x.1, function(e) {
+               if(class(e) == "yml_blank")
+                   e[[1]] = c("yml_blank")
+               else
+                   e = e})
 
-####    experiments
+    
+    # why need?
+    x.3  <- unlist(x.2)
+    str(x.3)
+    
+
+
+    ## strip off numbers of end of file names
+    x.4  <- x.3
+    x.4
+    names(x.4)  <- sub("[0-9]+$", "", names(x.3))
+    x.4
+
+
+    dt.1  <- data.table(file = names(x.4), tags = x.4)
+    dt.1 %>% head(n=30L)
+    View(dt.1)
+
+## KEEP rows with TAGS or yml_blank 
+    {
+    dt.1$tags
+    dt.2  <- dt.1[grep(tags, pattern="^TAGS:|^yml_blank"),]
+    dt.2 %>% head(n=20L)
+    str(dt.2)
+
+    dt.4  <- dt.2
+}
+
+## remove leading ^TAGS: (Explain need for `unlist`)
+## dt.5 should be clean, but not normalized
+{
+    dt.5  <- dt.4[, .(file, tags = 
+                      unlist(lapply(tags, function(e) sub(pattern="^TAGS:", replacement="", x=e))))]
+    dt.5
+    str(dt.5)
+
+}
+        end  <- Sys.time()
+        print(end - begin)
+}  ## END timed 
+
+####  dt.5 is clean, but not normalized.
+###   Summary:  if no YAML, file has "yml_blank"
+###   IF TAGS:, but blank means  TAGS: line has no content.
+###   IF YAML, but no TAGS: line:   think it was removed!
+{  ## view dt <C-Q> to close
+  View(dt.5)
+}
+
+
+## NORMALIZE:  RETURN DT
+    {
+    split_up  <- function(e) strsplit(e,",") 
+    dt.6  <- dt.5[, .(tags = unlist(lapply(tags, split_up) )), by=file]
+    }
+
+## REMOVE White Space, return CLEAN `files` as DT 
 {
 
-    content  <- unlist(the_yaml)
-    ##  the_yaml[[1]]  only returns first element
-    head(content)  # named atomic character vector!
-    dt  <- data.table(names(content), unname(content))
-    dt
-
-}
-}
-
-
-{ ## extract TAGS content
-
-    ## the_yaml is named list, one per file.  Contents of each element is char[],  one for each line of
-      ## YAML.  
-      ##
-    # in base:   return named char[], with NA if file has no TAGS line
-    # default: unlist removes names 
-    TAGS <- unlist(lapply(the_yaml, get_t), use.names =T)
-    TAGS[1:20]
-    length(TAGS)
-    is.character(TAGS)
+        trim.lead  <- function(x) sub("^\\s+", "", x)
+        trim.after  <- function(x) sub("\\s+$","",x)
+        dt.7  <- dt.6[, .(file, tags = sapply(tags, trim.lead))] 
+        str(dt.7)
+        dt.7  %>% head(n=20L)
+        files  <- dt.7
 }
 
-### START
-
-{ ## setup in dt
-  dt  <- data.table(TAGS = TAGS, names=names(TAGS))
- str( dt)
- length(dt)
-
- dt[1:3,]
-}
-
-end  <- Sys.time()
-print(end - begin)
-dt[!is.na(TAGS),]
-dt
-}
-
-{  ## experiments with kable
-  # output html
-  kable(dt)
-  kable(dt,format="pipe", align = "l", caption = "TAGs and files")  %>% head(n=100L)
-
-  ## best
-  View(dt)
-
-# remove
-if (F) {
-# Step 1, remove NULL
-f  <- function(e) ifelse(is.null(e), return(NA), return(e))
-dt  <- dt[,.(files, TAGS = lapply(TAGS, f)) ]
-dt
-}
-
-## resume
+### FINALLY, return DT for `tags`, a lookup table  
 {
-# Step 2, NA_character_
-# Step 2  use NA_character_ version of NA
-#base::strsplit() works with NA_character_, but not NA
-f  <- function(e) ifelse(is.character(e),return(e), return(c(NA_character_)))
-dt  <- dt[, .(names, TAGS = lapply(TAGS, f)) ]
-dt
+        dt.8  <- dt.7[,.SD , .SDcols=c("tags")]
+        setkey(dt.8, tags)
+        key(dt.8)
+        ## also check with tables()
+        dt.8 %>% head(n=30L) 
 
-# Step 3, split and normalize
-split_up  <- function(e) base::strsplit(e, split="[,]")
-dt  <- dt[, .(TAGS = unlist(lapply(TAGS, split_up) )) , by=names]
-head(dt, n=100L)
-dt[, .N]
-dt[, .N, by = TAGS]
+        ##  apparently setkey does not remove NA
+        tags  <- dt.8
+        tags
+        #### Capitals before small letters!
+        #  tags[order(tags)]
+        tags[, tags]  # as character vector
+        tags %>% head(n=10L)
+}
 
-## SAVE and RETRIEVE
+
+###  Now have 2 data.tables:   
+    *   files has the data (file names and tags) and,
+    *   tags is the look up table.
+
+## SAVE and RETRIEVE( redo)
 {
-file_name  <- paste0("TAGS_", format(Sys.Date(), "%d%b%Y"))
-file_name
+        file_name  <- paste0(c("FILES_", "TAGS_"), format(Sys.Date(), "%d%b%Y"))
 
-# For coding, R internals:
-# save/load save one or more objects into environment
+        # For coding, R internals:
+        # save/load save one or more objects into environment
 
-# For users:
-# saveRDS/readRDS save ONE object, possible retrieve in different name
-saveRDS(dt, file_name)
+        # For users:
+        # saveRDS/readRDS save ONE object, possible retrieve in different name
+        saveRDS(tags, file_name[2])
+        saveRDS(files, file_name[1])
 
-# retrieve
-lines  <- readRDS(file_name)
-lines
+        # retrieve
+        readRDS(file_name[1])
+        readRDS(file_name[2])
 }
 
-## for each TAG, list the files
-  ## list by TAGS - something wRONG - 
-  dt[!is.na(TAGS), ][order(TAGS),.(TAGS, names) ]
+###  Problem, want in order of tags, then file
+{
+    ##  file order, then tag
+        files[order(tags, file)] %>% head(n=20L)
 
-dt[, print(.SD), by=.(TAGS)]
-
-## list by file names
-  dt[!is.na(TAGS), sort(names)]
-
-# number of TAGS per file
-  dt[!is.na(TAGS), .N,  by=names] []
-
-dt
-names(dt)
+    ##  tags (caps first), then files
+        files[,.SD, by = .(tags)][order(tags)]  %>%  head(n=30L)
 
 
+        ## list by file names
+        dt[!is.na(TAGS), sort(names)]
+
+        # number of TAGS per file
+        dt[!is.na(TAGS), .N,  by=names] []
+
+        dt
+        names(dt)
 }
+
+```
+
